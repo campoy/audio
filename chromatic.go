9f2a62dc5a3b5210de673060b5512d893d6a1ee0
Francesc Campoy <campoy@golang.org>
2013-12-03 21:14:57 -0500

    making it more go-idiomatic

--- chromatic.go
+++ chromatic.go
@@ -9,60 +9,72 @@ import (
 
 const sampleRate = 44100
 
+var step = math.Pow(2, 1.0/12)
+
 func main() {
 	portaudio.Initialize()
 	defer portaudio.Terminate()
 
-	freq, step := 55., math.Pow(2, 1.0/12)
-
 	// Multiple goroutines playing music, yay!
-	go func() {
-		s := newSine(freq, sampleRate)
-		chk(s.Start())
-		for {
-			time.Sleep(250 * time.Millisecond)
-			s.step *= step
+	Mchord := []float64{0, 4, 7, 12}
+
+	ss := make([]*Sine, 0, len(Mchord))
+	for _, f := range Mchord {
+		s, err := newSine(220*math.Pow(step, f), sampleRate)
+		if err != nil {
+			panic(err)
 		}
-		chk(s.Stop())
-		s.Close()
-	}()
+		defer s.End()
+		ss = append(ss, s)
+	}
 
-	go func() {
-		s := newSine(freq, sampleRate)
-		chk(s.Start())
-		for {
-			time.Sleep(500 * time.Millisecond)
-			s.step *= step
+	beat := time.Tick(500 * time.Millisecond)
+	quit := time.After(10 * time.Second)
+
+	for {
+		select {
+		case <-quit:
+			return
+		case <-beat:
+			for _, s := range ss {
+				s.Transpose(1)
+			}
 		}
-		chk(s.Stop())
-		s.Close()
-	}()
-
-	<-time.After(10 * time.Second)
-}
-
-type sine struct {
-	*portaudio.Stream
-	step, phase float64
-}
-
-func newSine(freq, sampleRate float64) *sine {
-	s := &sine{step: freq / sampleRate}
-	var err error
-	s.Stream, err = portaudio.OpenDefaultStream(0, 1, sampleRate, 0, s.processAudio)
-	chk(err)
-	return s
-}
-
-func (g *sine) processAudio(out [][]float32) {
-	for i := range out[0] {
-		out[0][i] = float32(math.Sin(2 * math.Pi * g.phase))
-		_, g.phase = math.Modf(g.phase + g.step)
 	}
 }
 
-func chk(err error) {
+type Sine struct {
+	str         *portaudio.Stream
+	step, phase float64
+}
+
+func newSine(freq, sampleRate float64) (*Sine, error) {
+	s := &Sine{step: freq / sampleRate}
+	str, err := portaudio.OpenDefaultStream(0, 1, sampleRate, 0, s.processAudio)
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	if err := str.Start(); err != nil {
+		return nil, err
+	}
+	s.str = str
+	return s, nil
+}
+
+func (s *Sine) Transpose(n int) {
+	s.step *= math.Pow(step, float64(n))
+}
+
+func (s *Sine) End() error {
+	if err := s.str.Stop(); err != nil {
+		return err
+	}
+	return s.str.Close()
+}
+
+func (s *Sine) processAudio(out [][]float32) {
+	for i := range out[0] {
+		out[0][i] = float32(math.Sin(2 * math.Pi * s.phase))
+		_, s.phase = math.Modf(s.phase + s.step)
 	}
 }
